7113fe45cf14c44d897c5085da83b490afd8300a
Chino <ronaldcubillo92@gmail.com>
2019-03-04 10:25:12 -0600

    Added new attr and the game is working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,10 @@
-var topics = ["Cat", "Dog", "Mosquitoe", "Fly"];
+var videogames = ["Cat", "Dog", "Mosquitoe", "Fly"];
 
 
  // Function for dumping the JSON content for each button into the div
  function displayAnimal() {
+       //Emptys the gifs div before anything
+       $("#gifs-appear-here").empty();
 
     var animal = $(this).attr("data-name");
     //adding the queryURL with my key and limit of 10 gifts
@@ -23,8 +25,14 @@ var topics = ["Cat", "Dog", "Mosquitoe", "Fly"];
             var p = $("<p>").text("Rating: " + results[i].rating);
             // Creating and storing an image tag
             var animalImage = $("<img>");
-            // Setting the src attribute of the image to a property pulled off the result item
-            animalImage.attr("src", results[i].images.fixed_height.url);
+            // Setting the src attribute of the image to a property pulled off the result item. Using
+            //fixed_height_small_still to get a static image.
+            animalImage.attr("src", results[i].images.fixed_height_still.url);
+            animalImage.attr("data-still" , results[i].images.fixed_height_still.url);
+            animalImage.attr("data-animate" , results[i].images.fixed_height.url);
+            animalImage.attr("data-state" , "still");
+            animalImage.attr( "id" , "gifImages");
+
             // Appending the paragraph and image tag to the animalDiv
             animalDiv.append(p);
             animalDiv.append(animalImage);
@@ -33,30 +41,28 @@ var topics = ["Cat", "Dog", "Mosquitoe", "Fly"];
           }
     });
   }
-  
 
- // Function for displaying movie data
+
+ // Function for displaying videogames data
  function renderButtons() {
     // Deleting the buttons prior to adding new movies
     // (this is necessary otherwise you will have repeat buttons)
     $("#buttons-view").empty();
     // Looping through the array of movies
-    for (var i = 0; i < topics.length; i++) {
+    for (var i = 0; i < videogames.length; i++) {
       // Then dynamically generating buttons for each movie in the array
       // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
       var a = $("<button>");
       // Adding a class of movie to our button
       a.addClass("animal");
       // Adding a data-attribute
-      a.attr("data-name", topics[i]);
+      a.attr("data-name", videogames[i]);
       // Providing the initial button text
-      a.text(topics[i]);
+      a.text(videogames[i]);
       // Adding the button to the buttons-view div
       $("#buttons-view").append(a);
     }
   }
-
-
 
    // This function handles events where one button is clicked
    $("#add-animal").on("click", function(event) {
@@ -66,20 +72,33 @@ var topics = ["Cat", "Dog", "Mosquitoe", "Fly"];
     var animal = $("#animal-input").val().trim();
 
     // Adding the movie from the textbox to our array
-    topics.push(animal);
-    console.log(topics);
+    videogames.push(animal);
+    console.log(videogames);
 
     // Calling renderButtons which handles the processing of our movie array
     renderButtons();
   });
 
-     // This function handles events where one button is clicked
-     $("div").on("click", function(event) {
-        alert("being  clicked");
-      });
+  function clicker () {
+    var state = $(this).attr("data-state");
+    console.log(state);
+    
+    if (state === "still"){
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    }
+    else{
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+      n++;
+    console.log(n);
+    }
+ };
 
 
-  // Function for displaying the movie info
+  $(document).on("click", "#gifImages", clicker);
+
+  // Function for displaying the topic info
   // Using $(document).on instead of $(".movie").on to add event listeners to dynamically generated elements
   $(document).on("click", ".animal", displayAnimal);
 
